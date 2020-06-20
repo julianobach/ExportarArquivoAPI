a449b97d21d0055477bb8acae0aa35a788fe7f42
julianobach <julianobach@gmail.com>
2020-06-20 17:36:10 -0300

    Add files via upload

---
+++ WebGenerateFile/WebGenerateFile/Controllers/ValuesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebGenerateFile.Service;
+
+namespace WebGenerateFile.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        [Route("ObterArquivo")]
+        public IActionResult ExportCode(string tabelaPlural, string tabelaSigular, string dominio, string controllerAPI, string controllerWeb, string viewIndex
+                                , string viewComponentCampos, string viewComponentDelete, string viewComponentLista)
+        {
+            WebExportTools webExportTools = new WebExportTools();
+ 
+            dominio = System.IO.File.ReadAllText(dominio);
+            controllerAPI = System.IO.File.ReadAllText(controllerAPI);
+            controllerWeb = System.IO.File.ReadAllText(controllerWeb);
+            viewIndex = System.IO.File.ReadAllText(viewIndex);
+            viewComponentCampos = System.IO.File.ReadAllText(viewComponentCampos);
+            viewComponentDelete = System.IO.File.ReadAllText(viewComponentDelete);
+            viewComponentLista = System.IO.File.ReadAllText(viewComponentLista);
+
+
+            var resultado = webExportTools.GerarArquivo(tabelaPlural, tabelaSigular, dominio, controllerAPI, controllerWeb, viewIndex, viewComponentCampos, viewComponentDelete, viewComponentLista);
+
+            const string contentType = "application/zip";
+            HttpContext.Response.ContentType = contentType;
+            var result = new FileContentResult(System.IO.File.ReadAllBytes(resultado), contentType)
+            {
+                FileDownloadName = $"{tabelaPlural}_{DateTime.Now.ToString("yyyyMMddHHmmss")}.zip"
+            };
+
+            webExportTools.LimparArquivos(resultado, tabelaPlural);
+            return result;
+
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebGenerateFile/WebGenerateFile/Service/WebExportTools.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebGenerateFile.Service
+{
+    public class WebExportTools
+    {
+        private string defaultDirectory = @"C:\temp";
+        private string defaultDestinationDirectory = @"C:\temp\ZipFile";
+        public string GerarArquivo(string tabelaPlural, string tabelaSigular, string dominio, string controllerAPI, string controllerWeb, string viewIndex
+                                , string viewComponentCampos, string viewComponentDelete, string viewComponentLista)
+        {
+
+            try
+            {
+                if (!string.IsNullOrEmpty(tabelaPlural) && !string.IsNullOrEmpty(tabelaSigular))
+                {
+                    string path = $"{defaultDirectory}\\{tabelaPlural}";
+                    if (System.IO.Directory.Exists(path))
+                        System.IO.Directory.Delete(path, true);
+
+                    System.IO.Directory.CreateDirectory(path);
+
+                    if (!string.IsNullOrEmpty(dominio))
+                    {
+                        System.IO.Directory.CreateDirectory($"{path}\\{tabelaPlural}Dominio");
+                        System.IO.File.WriteAllText($"{path}\\{tabelaPlural}Dominio\\{tabelaPlural}.cs", dominio);
+                    }
+
+                    if (!string.IsNullOrEmpty(controllerAPI))
+                    {
+                        System.IO.Directory.CreateDirectory($"{path}\\{tabelaPlural}ControllerAPI");
+                        System.IO.File.WriteAllText($"{path}\\{tabelaPlural}ControllerAPI\\{tabelaPlural}.cs", controllerAPI);
+                    }
+
+                    if (!string.IsNullOrEmpty(controllerWeb))
+                    {
+                        System.IO.Directory.CreateDirectory($"{path}\\{tabelaPlural}ControllerWeb");
+                        System.IO.File.WriteAllText($"{path}\\{tabelaPlural}ControllerWeb\\{tabelaPlural}.cs", controllerWeb);
+                    }
+
+                    if (!string.IsNullOrEmpty(viewIndex))
+                    {
+                        System.IO.Directory.CreateDirectory($"{path}\\{tabelaPlural}Views\\{tabelaPlural}");
+                        System.IO.File.WriteAllText($"{path}\\{tabelaPlural}Views\\{tabelaPlural}\\Index.cs", viewIndex);
+                    }
+
+                    if (!string.IsNullOrEmpty(viewComponentCampos))
+                    {
+                        System.IO.Directory.CreateDirectory($"{path}\\{tabelaPlural}Views\\{tabelaPlural}\\Components\\Vc{tabelaSigular}Campos");
+                        System.IO.File.WriteAllText($"{path}\\{tabelaPlural}Views\\{tabelaPlural}\\Components\\Vc{tabelaSigular}Campos\\Default.cshtml", viewComponentCampos);
+                    }
+
+                    if (!string.IsNullOrEmpty(viewComponentDelete))
+                    {
+                        System.IO.Directory.CreateDirectory($"{path}\\{tabelaPlural}Views\\{tabelaPlural}\\Components\\Vc{tabelaSigular}Deletar");
+                        System.IO.File.WriteAllText($"{path}\\{tabelaPlural}Views\\{tabelaPlural}\\Components\\Vc{tabelaSigular}Deletar\\Default.cshtml", viewComponentDelete);
+                    }
+
+                    if (!string.IsNullOrEmpty(viewComponentLista))
+                    {
+                        System.IO.Directory.CreateDirectory($"{path}\\{tabelaPlural}Views\\{tabelaPlural}\\Components\\Vc{tabelaSigular}Lista");
+                        System.IO.File.WriteAllText($"{path}\\{tabelaPlural}Views\\{tabelaPlural}\\Components\\Vc{tabelaSigular}Lista\\Default.cshtml", viewComponentLista);
+                    }
+                    System.IO.Stream stream = new System.IO.MemoryStream();
+
+                    var fileName = $"{tabelaPlural}{DateTime.Now.ToString("yyyyMMddHHmmss")}.zip";
+                    var destinationFile = $"{defaultDestinationDirectory}\\{fileName}";
+                    ZipFile.CreateFromDirectory(path, destinationFile);
+
+
+                    
+
+                    return destinationFile;
+
+                }
+                else
+                {
+                    return "Singular ou Plural não informados";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public void LimparArquivos(string filePath, string tabelaPlural)
+        {
+            string path = $"{defaultDirectory}\\{tabelaPlural}";
+            if (System.IO.Directory.Exists(path))
+                System.IO.Directory.Delete(path, true);
+
+            if (System.IO.File.Exists(filePath))
+                System.IO.File.Delete(filePath);
+        }
+    }
+
+}
